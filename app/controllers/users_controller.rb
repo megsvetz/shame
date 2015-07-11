76b1084752063d6dd6dfc372460cd7c71e3c0e7b
Meg Svetz <megsvetz@gmail.com>
2015-07-11 13:00:53 -0600

    styling

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+    # @categories = Category.order(created_at: :desc)
+    @categories = Category.order(updated_at: :desc).paginate(:page => params[:page], :per_page => 6)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:notice]="Person was updated successfully!"
+      redirect_to(users_path)
+    else
+      flash[:error]="Person was not updated."
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    # @categories = Category.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations' }
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :categories do
