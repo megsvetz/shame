a5e21bbfe381cb824f6379436be4389001eee07a
Meg Svetz <megsvetz@gmail.com>
2015-07-11 09:36:03 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :categories do
+    resources :goals
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
