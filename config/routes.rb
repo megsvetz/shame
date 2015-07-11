afb7a72de6a59a93b1e6e86c2832d558b9b6aa2f
reecebisel <reecebisel@gmail.com>
2015-07-11 10:05:11 -0600

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'posts/index'
 
   get 'posts/new'
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
