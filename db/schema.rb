213d3693090a014eb6b3e2d349046d0744e578b4
Meg Svetz <megsvetz@gmail.com>
2015-07-11 14:14:15 -0600

    added user belongs to in posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711161621) do
+ActiveRecord::Schema.define(version: 20150711201110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150711161621) do
     t.string   "post_pic_content_type"
     t.integer  "post_pic_file_size"
     t.datetime "post_pic_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
